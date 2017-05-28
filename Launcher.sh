cac81a778c9e6f9f7e2fb27d9efee88d64d841e6
Dmitriy Safronov <zimniy@cyberbrain.pw>
2017-05-28 05:39:08 +0300

    Small fixes

--- Launcher.sh
+++ Launcher.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Make sure only root can run our script
+# Script should be runned as root
 if [ "$(id -u)" != "0" ]; then
     sudo -u root -H -b $0 $@
     exit 0
 fi
 
 if [ -x "$1" ]; then
-    logger -t "Launcher" "Executng [$@] as root..."
+    logger -t "Launcher" "Executing [$@] as root..."
     $@ &
 else
     logger -t "Launcher" "Skipping execution of [$@] - not an executable..."
