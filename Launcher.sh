4b5f0f2b96881075e36c626b3cd5a520f85d3250
Dmitriy Safronov <zimniy@cyberbrain.pw>
2017-05-28 05:34:52 +0300

    Executable bit needed

--- Launcher.sh
+++ Launcher.sh
@@ -2,9 +2,13 @@
 
 # Make sure only root can run our script
 if [ "$(id -u)" != "0" ]; then
-    sudo -u root -H -b bash $0 $@
+    sudo -u root -H -b $0 $@
     exit 0
 fi
 
-logger -t "Launcher" "Executng [$@] as root..."
-bash $@ &
+if [ -x "$1" ]; then
+    logger -t "Launcher" "Executng [$@] as root..."
+    $@ &
+else
+    logger -t "Launcher" "Skipping execution of [$@] - not an executable..."
+fi
