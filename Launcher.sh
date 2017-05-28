c713086e7f58f40bd0da6488b162bfa36570f6bc
Dmitriy Safronov <zimniy@cyberbrain.pw>
2017-05-28 05:29:27 +0300

    Bash as glue

--- Launcher.sh
+++ Launcher.sh
@@ -2,9 +2,9 @@
 
 # Make sure only root can run our script
 if [ "$(id -u)" != "0" ]; then
-    sudo -u root -H -b $0 $@
+    sudo -u root -H -b bash $0 $@
     exit 0
 fi
 
 logger -t "Launcher" "Executng [$@] as root..."
-$@ &
+bash $@ &
