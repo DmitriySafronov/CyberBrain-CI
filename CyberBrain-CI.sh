aedea87f63412cf9e42611ecf3846480cdc1c90c
Dmitriy Safronov <zimniy@cyberbrain.pw>
2017-05-28 14:35:26 +0300

    Wrapper & Launcher reworked 2

--- CyberBrain-CI.sh
+++ CyberBrain-CI.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Script should be runned as root
+# Script should be runned as "cyberbrain-ci"
 if [ "${USER}" != "cyberbrain-ci" ]; then
-    sudo -u cyberbrain-ci -H -b $0 $@
+    sudo -u cyberbrain-ci -H -b $0 $1
     exit 0
 fi
 
-if [ -x "~/recipes/$1" ]; then
-    logger -t "CyberBrain-CI" "Executing [$@]..."
+if [ -x "${HOME}/recipes/$1" ]; then
+    logger -t "CyberBrain-CI" "Executing [$1]..."
     eval `ssh-agent`
-    ssh-add ~/.ssh/id_rsa
-    ~/recipes/$@
+    ssh-add "${HOME}/.ssh/id_rsa"
+    "${HOME}/recipes/$1"
     ssh-agent -k
 else
-    logger -t "CyberBrain-CI" "Skipping execution of [$@] - not an executable..."
+    logger -t "CyberBrain-CI" "Skipping execution of [$1] - not an executable..."
 fi
